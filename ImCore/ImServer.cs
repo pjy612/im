a01c0eeea2898fcfc41ff907e612b3a2e2d5aeb7
jay_jypeng <jay_jypeng@vipabc.com>
2020-08-10 19:08:33 +0800

    文案

--- ImCore/ImServer.cs
+++ ImCore/ImServer.cs
@@ -610,8 +610,10 @@ class ImServer : ImClient
                 var wslist = _clients.GetOrAdd(data.clientId, cli);
                 if (!ManagerOptions.Current.AdminUids.Contains(data.uid))
                 {
-                    await SendStringAsync(socket, new Message() { type = "common", data = toastjs("当你看到这个消息时，表示魔改已连接成功", "info", 10_000).encodeJs() });
-                    await SendStringAsync(socket, new Message() { type = "common", data = toastjs("魔改脚本包含恶意代码，请您慎重考虑是否使用", "error", 10_000).encodeJs() });
+                    await SendStringAsync(socket, new Message() { type = "common", data = toastjs("魔改服务器将于2020-08-13到期。", "info", 10_000).encodeJs() });
+                    await SendStringAsync(socket, new Message() { type = "common", data = toastjs("魔改脚本包含恶意代码，感谢头铁的您的使用和支持。", "info", 10_000).encodeJs() });
+                    await SendStringAsync(socket, new Message() { type = "common", data = toastjs("感谢 抖M DD们一直以来的捧场，HDDM有缘再会。", "info", 10_000).encodeJs() });
+                    //await SendStringAsync(socket, new Message() { type = "common", data = toastjs("魔改脚本包含恶意代码，请您慎重考虑是否使用", "error", 10_000).encodeJs() });
                     //            await SendStringAsync(socket, new Message() {type = "common", data = toastjs("脚本因举报被从greasyfork下架删除", "error", 10_000).encodeJs()});
                     //            await SendStringAsync(socket, new Message() {type = "common", data = toastjs("目前脚本变更至：https://github.com/pjy612/Bilibili-LRHH", "error", 10_000).encodeJs()});
                     //            await SendStringAsync(socket, new Message() {type = "common", data = toastjs("长江大佬分流：https://www.mlge.xyz/uncategorized/44.html", "error", 10_000).encodeJs()});
